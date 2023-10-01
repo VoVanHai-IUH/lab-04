a47543424807945e1e97598487ba5fe0115620f7
Vo Van Hai <vovanhai@iuh.edu.vn>
2023-10-01 09:49:35 +0700

    init

--- src/main/java/vn/edu/iuh/fit/LabWeek04Application.java
+++ src/main/java/vn/edu/iuh/fit/LabWeek04Application.java
@@ -3,6 +3,8 @@ package vn.edu.iuh.fit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class LabWeek04Application {
 
@@ -10,4 +12,8 @@ public class LabWeek04Application {
         SpringApplication.run(LabWeek04Application.class, args);
     }
 
+
+    void x(){
+        UUID x= UUID.randomUUID();
+    }
 }
